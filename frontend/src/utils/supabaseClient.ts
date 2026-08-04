7d28c441c15186ab10981cdf9141ac3d605f3829
aiandi49 <ai.agent.lamar@gmail.com>
2026-08-03 18:14:31 -0700

    Update Supabase client URL and anon key

--- frontend/src/utils/supabaseClient.ts
+++ frontend/src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zubuqhdelzdujuwtcyzx.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1YnVxaGRlbHpkdWp1d3RjeXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1OTExNDUsImV4cCI6MjA5MjE2NzE0NX0.B5hB3VA8xtWoySj6765QjbtHpt6cCsa_hXOGUGZ0e6w';
+const SUPABASE_URL = 'https://msuqflssqixsplcearxr.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zdXFmbHNzcWl4c3BsY2VhcnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMzU3NDgsImV4cCI6MjA4OTcxMTc0OH0.BlrDiIu2YdDB7oHJ0fFiSEiBDQGvHCFAUaysR6O24zk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
